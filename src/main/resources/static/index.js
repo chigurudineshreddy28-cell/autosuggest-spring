18ff40dd7476169fb36650b46cd5dd2d9531b632
chiguru dinesh reddy <chigurudineshredddy28@gmail.com>
2026-07-18 12:08:57 +0530

    implemented toggleuser

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -1,2 +1,27 @@
-//alert(  "hello mavaa")
-console.log("hello mava")
+
+var users = [
+    {
+        "name": "Dinesh Reddy",
+        "gender": "Male",
+        "image":"john.png"
+    },
+        {
+        "name": "Ruku",
+        "gender": "female",
+        "image":"image.png"
+    }
+]
+var id=0;
+function ToggleUser() {
+    id=(id+1)%2
+    //get the elements
+    // Image 1
+    var userImage= document.getElementById("user-image");
+    userImage.src=users[id].image;
+    // Name 2
+    var userName= document.getElementById("user-name");
+    userName.innerHTML=users[id].name;
+    // gender 3
+    var userGender= document.getElementById("user-gender");
+    userGender.innerHTML=users[id].gender;  
+  }
